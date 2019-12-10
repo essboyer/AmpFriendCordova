300a73e62c533b3cf2a3f38013ceb9adb03c7730
Sean Boyer <seanboyer@Seans-MBP2.lan>
2019-12-10 16:36:51 -0400

    First one. It's in a working state but is messy. All works except the power to voltage feature.

---
+++ platforms/ios/www/js/maths.js
@@ -0,0 +1,25 @@
+function findPFromRMS(rmsVolts, load)
+{
+    return Math.round((((rmsVolts * rmsVolts) / load) * 100)) / 100;
+}
+
+function findPFromPeak(peakVolts, load)
+{
+    var rmsVolts = peak2RMS(peakVolts);
+    return findPFromRMS(rmsVolts, load);
+}
+
+function findRMSFromP(p, load)
+{
+    return Math.round(Math.sqrt(p * load));
+}
+
+function peak2RMS(peakVolts)
+{
+    return Math.round(0.3535 * peakVolts);
+}
+
+function rms2Peak(rmsVolts)
+{
+    return Math.round(rmsVolts * (1.414 * 2));
+}
